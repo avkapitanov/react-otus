79d161ff45c199da9251f55a3f15041e22509d01
Artem Kapitanov <avkapitanov@yandex.ru>
2022-02-07 20:54:08 +0300

    feat: add App component

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,8 @@ module.exports = {
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/settings/JestSetup.js'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
+  },
 };
